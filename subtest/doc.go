b4544476cdb55202380f394ee9f767c8bf1ee504
Koichi Shiraishi <zchee.io@gmail.com>
2018-10-11 07:23:44 +0900

    subtest: add subtest package which utility of sub-test testing

---
+++ subtest/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2018 The spinctl Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package subtest provides a TestContext to subtests which handles cleanup, and
+// provides a testing.TB, and context.Context.
+// This package was inspired by github.com/frankban/quicktest.
+package subtest
